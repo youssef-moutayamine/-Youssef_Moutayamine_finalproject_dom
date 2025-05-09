c520a105ebbcf19d9dd8db7fa39c030d7e9c441e
youssef-moutayamine <moutayamineyoussef6@gmail.com>
2025-05-09 14:20:54 +0100

    menu sections done

--- public/js/app.js
+++ public/js/app.js
@@ -20,11 +20,33 @@ hide.addEventListener("click", () => {
     document.body.style.overflow = "auto"
     pack.classList.remove("backdrop")
 })
-submit.addEventListener("click", () => {
-    myModal.classList.add("close")
-    document.body.style.overflow = "auto"
-    pack.classList.remove("backdrop")
-})
 
 
-// carousel 
+
+// filter buttons
+
+document.addEventListener('click',  () => {
+    let filterButtons = document.querySelectorAll('.filter-button');
+    let menuItems = document.querySelectorAll('.menu-list .items');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+
+            this.classList.add('active');
+
+            const filterValue = this.getAttribute('data-filter');
+
+            menuItems.forEach(item => {
+                const category = item.querySelector('.pictures').getAttribute('data-name');
+
+                if (filterValue === 'all' || filterValue === category) {
+                    item.style.display = 'flex'; 
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    });
+});
